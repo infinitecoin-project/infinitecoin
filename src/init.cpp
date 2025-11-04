c3b6dab887019b8ebccd2a33bd42f4a0beacefc8
infinitecoin <63966367+infinitecoin-project@users.noreply.github.com>
2025-11-04 23:49:02 +0800

    Fix: Infinitecoin brand project consistency

--- src/init.cpp
+++ src/init.cpp
@@ -192,7 +192,7 @@ void Shutdown()
     /// for example if the data directory was found to be locked.
     /// Be sure that anything that writes files or flushes caches only does this if the respective
     /// module was initialized.
-    RenameThread("bitcoin-shutoff");
+    RenameThread("infinitecoin-shutoff");
     mempool.AddTransactionsUpdated(1);
 
     StopHTTPRPC();
@@ -614,7 +614,7 @@ void CleanupBlockRevFiles()
 void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
 {
     const CChainParams& chainparams = Params();
-    RenameThread("bitcoin-loadblk");
+    RenameThread("infinitecoin-loadblk");
 
     {
     CImportingNow imp;
