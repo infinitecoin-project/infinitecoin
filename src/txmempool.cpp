11a43dc29723fd17b3a5a555d08994850b37a01a
infinitecoin <63966367+infinitecoin-project@users.noreply.github.com>
2025-11-05 23:25:11 +0800

    Remove redundant explicitly defined copy ctors
    CFeeRate and CTxMemPoolEntry have explicitly defined copy ctors which has
    the same functionality as the implicit default copy ctors which would
    have been generated otherwise.
    
    Besides being redundant, it violates the rule of three
    (see https://en.wikipedia.org/wiki/Rule_of_three_(C%2B%2B_programming) ).
    (Of course, the rule of three doesn't -really- cause a resource
    management issue here, but the reason for that is exactly that there is
    no need for an explicit copy ctor in the first place since no resources
    are being managed).
    
    CFeeRate has an explicitly defined copy ctor which has the same
    functionality as the implicit default copy ctor which would have been
    generated otherwise.
    
    Reference from dogecoin project

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -44,11 +44,6 @@ CTxMemPoolEntry::CTxMemPoolEntry(const CTransactionRef& _tx, const CAmount& _nFe
     nSigOpCostWithAncestors = sigOpCost;
 }
 
-CTxMemPoolEntry::CTxMemPoolEntry(const CTxMemPoolEntry& other)
-{
-    *this = other;
-}
-
 double
 CTxMemPoolEntry::GetPriority(unsigned int currentHeight) const
 {
